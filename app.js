17c4c9346d134005d2db72e3bd26013055651045
mohsenpro54 <mohsengafsi123@hotmail.com>
2021-09-29 20:16:34 +0100

    Added a comment

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ const bookingRouter = require('./routes/bookingRoutes');
 
 const viewRouter = require('./routes/viewRoutes');
 
+// Start express app
 const app = express();
 
 app.set('view engine', 'pug');
